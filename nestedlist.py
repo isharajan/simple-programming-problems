505527a8a58cb51fdeab5dd87ffb72a21cc8ed56
isharajan <isharajan23@gmail.com>
2019-04-29 17:39:14 +0530

    added nestedlist.py

---
+++ nestedlist.py
@@ -0,0 +1,11 @@
+l = []
+for i in range(3):
+    l1 = []
+    name = raw_input("enter the name:")
+    score = float(raw_input("enter the score:"))
+    l1.append(name)
+    l1.append(score)
+    l.append(l1)     
+print(l)
+
+
